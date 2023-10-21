a5feed4c25607681cdd2520b710033d4160c6095
unknown <jiyuzoa@gmail.com>
2023-10-21 12:38:27 +0900

    231021

---
+++ Project4/소스1.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//
+//int main(void)
+//{
+//	int x1, y1, x2, y2, x3, y3;
+//	unsigned int area;
+//	printf("두 점의좌표 입력: ");
+//	scanf("%d %d %d %d", &x1,&y1, &x2, &y2);
+//	x3 = x2 - x1;
+//	y3 = y2 - y1;
+//
+//	area = x3 * y3;
+//	printf("첫번째 좌표:%d %d \n", x1, y1);
+//	printf("두번째 좌표:%d %d \n", x2, y2);
+//	printf("직사각형 넓이:%d", area);
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	double num1, num2;
+//	printf("숫자입력: ");
+//	scanf("%lf %lf", &num1, &num2);
+//	printf("더하기: %lf, 빼기: %lf, 곱하기: %lf, 나누기: %lf", num1 + num2, num1 - num2, num1 * num2, num1 / num2);
+//	return 0;
+//
+//}
+//int main(void)
+//{
+//	int x;
+//
+//	printf("0~127사이 입력: ");
+//	scanf("%d", &x);
+//	printf("아스키코드: %c", x);
+//	return 0;
+//}
+//int main(void)
+//{
+//	printf("%f \n", 0.1234);
+//	printf("%e \n", 0.1234);
+//	printf("%f \n", 0.12345678);
+//	printf("%e \n", 0.12345678);
+//	return 0;
+//}
+int main(void)
+{
+	double d1 = 1.23e-3;
+	double d2 = 1.23e-4;
+	double d3 = 1.23e-5;
+	double d4 = 1.23e-6;
+
+	printf("%g \n", d1);
+	printf("%g \")
+}
